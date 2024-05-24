dd1b5bd043817076525b744e4691e8934e0d0775
Jason Rubiano <jasonmeca@gmail.com>
2024-05-23 22:20:44 -0500

    project added

---
+++ MovieList/MovieList/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MovieList
+//
+//  Created by JASS on 23/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieList/MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by JASS on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
